b44fb0df675c0640cf2020f261e099f220313726
ntuananhdevs <anhndt25@gmail.com>
2024-08-24 23:55:56 +0700

    update chat

--- src/components/chats/chat.jsx
+++ src/components/chats/chat.jsx
@@ -1,37 +1,135 @@
-import "./chat.css"
+import { useState, useEffect, useRef } from "react";
+import "./chat.css";
+import EmojiPicker from "emoji-picker-react";
 
 const Chat = () => {
+    const [open, setEmoji] = useState(false);
+    const [text, setText] = useState("");
+    const emojiRef = useRef(null);
+
+    const handleEmojiClick = (e) => {
+        setText((prev) => prev + e.emoji);
+    };
+
+    const handleClickOutside = (e) => {
+        if (emojiRef.current && !emojiRef.current.contains(e.target)) {
+            setEmoji(false);
+        }
+    };
+
+    useEffect(() => {
+        if (open) {
+            document.addEventListener("click", handleClickOutside);
+        } else {
+            document.removeEventListener("click", handleClickOutside);
+        }
+
+        return () => {
+            document.removeEventListener("click", handleClickOutside);
+        };
+    }, [open]);
+
     return (
-        <div className='chat'>
+        <div className="chat">
             <div className="top">
                 <div className="user">
-                    <img src="./avatar.png" alt="" />
+                    <img src="./avatar.png" alt="User avatar" />
                     <div className="text">
                         <span>Jane Doe</span>
                         <p>Lorem ipsum dolor sit amet</p>
                     </div>
                 </div>
                 <div className="icons">
-                    <img src="./phone.png" alt="" />
-                    <img src="./video.png" alt="" />
-                    <img src="./info.png" alt="" />
+                    <ion-icon name="call"></ion-icon>
+                    <ion-icon name="videocam"></ion-icon>
+                    <ion-icon name="alert-circle"></ion-icon>
                 </div>
             </div>
-            <div className="center"></div>
+
+            <div className="center">
+                <div className="message own">
+                    <div className="texts">
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            Ducimus ipsa beatae voluptas provident voluptatum quidem quae consectetur eos,
+                            explicabo numquam dignissimos quasi iste magnam quibusdam nulla magni cupiditate et tempora.
+                        </p>
+                        <span>1 min ago</span>
+                    </div>
+                </div>
+                <div className="message">
+                    <img src="./avatar.png" alt="" />
+                    <div className="texts">
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            Ducimus ipsa beatae voluptas provident voluptatum quidem quae consectetur eos,
+                            explicabo numquam dignissimos quasi iste magnam quibusdam nulla magni cupiditate et tempora.
+                        </p>
+                        <span>1 min ago</span>
+                    </div>
+                </div>
+                <div className="message own">
+                    <div className="texts">
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            Ducimus ipsa beatae voluptas provident voluptatum quidem quae consectetur eos,
+                            explicabo numquam dignissimos quasi iste magnam quibusdam nulla magni cupiditate et tempora.
+                        </p>
+                        <span>1 min ago</span>
+                    </div>
+                </div>
+                <div className="message">
+                    <img src="./avatar.png" alt="" />
+                    <div className="texts">
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            Ducimus ipsa beatae voluptas provident voluptatum quidem quae consectetur eos,
+                            explicabo numquam dignissimos quasi iste magnam quibusdam nulla magni cupiditate et tempora.
+                        </p>
+                        <span>1 min ago</span>
+                    </div>
+                </div>
+                <div className="message own">
+                    <div className="texts">
+                        <img src="./bg.jpg" alt="" />
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            Ducimus ipsa beatae voluptas provident voluptatum quidem quae consectetur eos,
+                            explicabo numquam dignissimos quasi iste magnam quibusdam nulla magni cupiditate et tempora.
+                        </p>
+                        <span>1 min ago</span>
+                    </div>
+                </div>
+            </div>
             <div className="bottom">
                 <div className="icons">
-                    <img src="./img.png" alt="" />
-                    <img src="./camera.png" alt="" />    
-                    <img src="./mic.png" alt="" />    
+                    <span><ion-icon name="images-outline"></ion-icon></span>
+                    <span><ion-icon name="camera-outline"></ion-icon></span>
+                    <span><ion-icon name="mic-outline"></ion-icon></span>
                 </div>
-                <input type="text" />
-                <div className="emoji">
-                    <img src="./emoji.png" alt="" />
+                <input
+                    type="text"
+                    placeholder="Type a message"
+                    value={text}
+                    onChange={(e) => setText(e.target.value)}
+                />
+                <div className="emoji" ref={emojiRef}>
+                    <img
+                        src="./emoji.png"
+                        alt="Open emoji picker"
+                        onClick={() => setEmoji((prev) => !prev)}
+                    />
+                    <div className="picked">
+                        {open && <EmojiPicker onEmojiClick={handleEmojiClick} />}
+                    </div>
                 </div>
-                <div className="sendButton">Send</div>
+                <div className="sendButton">
+                    <ion-icon name="send"></ion-icon>
+                </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Chat
+export default Chat;
+
--- src/components/detail/detail.jsx
+++ src/components/detail/detail.jsx
@@ -2,7 +2,47 @@ import "./detail.css"
 const detail = () => {
     return (
         <div className='detail'>
-            Detail
+            <div className="user">
+                <img src="./avatar.png" alt="" />
+                <h2>Jane Doe</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </div>
+            <div className="info">
+                <div className="option">
+                    <div className="title">
+                        <span>Chat Setting</span>
+                        <ion-icon name="chevron-up-outline"></ion-icon>
+                    </div>
+                </div>
+                <div className="option">
+                    <div className="title">
+                        <span>Privacy % help</span>
+                        <ion-icon name="chevron-up-outline"></ion-icon>
+                    </div>
+                </div>
+                <div className="option">
+                    <div className="title">
+                        <span>Share Photo</span>
+                        <ion-icon name="chevron-down-outline"></ion-icon>
+                    </div>
+                    <div className="photos">
+                        <div className="photoItem">
+                            <div className="photoDetail">
+                                <img src="./bg.jpg" alt="" />
+                                <span>Photo_2024-02-16_00-55-10.jpg</span>
+                            </div>
+                                <ion-icon name="cloud-download-outline"></ion-icon>
+                        </div>
+                    </div>
+                </div>
+                <div className="option">
+                    <div className="title">
+                        <span>Share File</span>
+                        <ion-icon name="chevron-up-outline"></ion-icon>
+                    </div>
+                </div>
+                <button>Block User</button>
+            </div>
         </div>
     )
 }
